fc5f3e0ad9672af7f7ce0ab65bbee36b59a5e9d1
Kiara Jarzinski <jarzinskikiara@gmail.com>
2026-09-10 09:40:39 -0300

    sobreescribo cambios de eri y ana

---
+++ frontend/src/Pages/HomePage/Socios.jsx
@@ -0,0 +1,10 @@
+import Loading from "../../Components/Loading";
+import Navbar from "../../Components/Navbar";
+
+export default function Socios() {
+    return <>
+        <Navbar />
+        <Loading />
+    </>
+
+}
