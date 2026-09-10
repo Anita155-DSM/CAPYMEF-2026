5287d4e42f85f6afd6f615f06ebd7b4015aa08e2
AnitaDSM-155 <anahiperezdsm@gmail.com>
2026-09-10 16:02:48 -0300

    modularice validarResultado, mismas lineas de codigo que utilizan todos los modulos de validator hasta ahora

--- backend/middlewares/validator/user.validator.js
+++ backend/middlewares/validator/user.validator.js
@@ -1,17 +1,18 @@
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import { validarResultado } from './validarResultado.js';
 
 // Middleware genérico que revisa si express-validator atrapó algún error
-export const validarResultado = (req, res, next) => {
-  const errores = validationResult(req);
-  if (!errores.isEmpty()) {
-    return res.status(400).json({
-      exito: false,
-      mensaje: 'Error de validación en los datos enviados.',
-      errores: errores.array().map(err => ({ campo: err.path, mensaje: err.msg }))
-    });
-  }
-  next();
-};
+// export const validarResultado = (req, res, next) => {
+//   const errores = validationResult(req);
+//   if (!errores.isEmpty()) {
+//     return res.status(400).json({
+//       exito: false,
+//       mensaje: 'Error de validación en los datos enviados.',
+//       errores: errores.array().map(err => ({ campo: err.path, mensaje: err.msg }))
+//     });
+//   }
+//   next();
+// };
 
 // ==========================================
 // Reglas para el Registro de Socio
